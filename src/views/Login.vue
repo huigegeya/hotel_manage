78ad2a77b2d4f8675a2d8c79392627f49e6a1289
huigegeya <774940833@qq.com>
2023-02-19 16:04:54 +0800

    login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,39 +1,64 @@
 <template>
-<div class="container">
-    <div class="login">
-        <div class="item">
-            <h2>酒店管理系统</h2>
-        </div>
-        <div class="item">
-            <span>账号：</span>
-            <el-input v-model="input" placeholder="Please input" />
-        </div>
-        <div class="item">
-            <span>密码：</span>
-            <el-input v-model="input" placeholder="Please input" />
-        </div>
-        <div class="item">
-            <span>记住我</span>
-            <el-radio label="1" size="large"></el-radio>
-        </div>
-        <div class="item">
-            <el-button type="success">Success</el-button>
-            <el-button>Default</el-button>
+    <div class="container">
+        <div class="login">
+            <div class="item">
+                <h2>酒店管理系统</h2>
+            </div>
+            <div class="item">
+                <span>账号：</span>
+                <el-input size="mini" v-model="input" placeholder="请输入账号" />
+            </div>
+            <div class="item">
+                <span>密码：</span>
+                <el-input size="mini" v-model="input" placeholder="请输入密码" />
+            </div>
+            <div>
+                <span>记住我： </span>
+                <el-radio size="mini"></el-radio>
+            </div>
+            <div class="item">
+                <el-button size="mini" type="success">登录</el-button>
+                <el-button size="mini">取消</el-button>
+            </div>
         </div>
     </div>
-</div>
-</template>
-
-<script>
-export default {
-name:'Login'
-}
-</script>
-
+    </template>
+    
+    <script>
+    export default {
+    name:'Login'
+    }
+    </script>
+    
 <style>
 .container{
     width: 100vw;
     height: 100vh;
     background: linear-gradient(to bottom,rgb(13,54,82),lightblue);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+} 
+.container .login{
+    width: 400px;
+    height: 220px;
+    border: 1px solid #ccc;
+    border-radius: 6px;
+    padding: 10px;
 }
+.container .login .item{
+    display: flex;
+    justify-content: center;
+    margin: 10px 0;
+    font:14px;
+}
+.container .login .item h2{
+    text-align: center;
+}
+.container .login .item span{
+    width: 55px;
+   
+
+}
+
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,24 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: true,
+  configureWebpack: {
+      module: {
+          rules: [
+            // 全局导入 scss
+            {
+              test: /\.scss$/,
+              exclude: /node_modules/,
+              use: [
+                {
+                  loader: 'sass-resources-loader',
+                  options: {
+                    resources: ['./src/styles/variables.scss']
+                  }
+                }
+              ]
+            }
+          ]
+        }
+    }
 })
